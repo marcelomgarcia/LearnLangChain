387d270529fe7e5b03cb93ccec2e255c3f691a78
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-12-31 06:25:32 +0000

    Document loader

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from langchain_community.document_loaders.csv_loader import CSVLoader
+
 def main():
     print("Hello from learnlangchain!")
 
+    loader = CSVLoader(file_path="data/metadata.csv")
+
+    data = loader.load()
+
+    print(data)
 
 if __name__ == "__main__":
     main()
